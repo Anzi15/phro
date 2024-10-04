e51e507c2349fd67b51e01525fa805f73ea83ada
Lemme.code <djam4343@gmail.com>
2024-10-04 15:57:01 +0500

    navbar done

---
+++ app/components/Button.jsx
@@ -0,0 +1,12 @@
+"use cleint"
+import React from 'react'
+
+export const themeRoundedBtn = ({text,href}) => {
+  return (
+    <Link href={`${href}`} className='bg-blue-800 text-white font-bold p-4'>
+        {text}
+    </Link>
+  )
+}
+
+
